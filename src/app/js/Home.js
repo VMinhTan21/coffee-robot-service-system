60ed3d35cf2488bbf83f5cd8272f67deb898aad9
VMinhTan <vienminhtanmd@gmail.com>
2023-06-18 14:52:46 +0700

    Update Home.js
    127.0.0.1

--- src/app/js/Home.js
+++ src/app/js/Home.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
 
   // ROS
   var ros = new ROSLIB.Ros({
-    url: 'https://10.3.10.199:9090'
+    url: 'https://127.0.0.1:9090'
   });
 
   ros.on('connection', function () {
