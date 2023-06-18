0b7d32dd390769625e02cde0920f25e246e03f1f
VMinhTan <vienminhtanmd@gmail.com>
2023-06-18 15:20:43 +0700

    Update Home.js
    9090

--- src/app/js/Home.js
+++ src/app/js/Home.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
 
   // ROS
   var ros = new ROSLIB.Ros({
-    url: 'wss://192.168.1.201:443'
+    url: 'wss://192.168.1.201:9090'
   });
 
   ros.on('connection', function () {
