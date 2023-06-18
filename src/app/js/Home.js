08df8070b3ffba46e3bdfbb6eacc41b8efcb001b
VMinhTan <vienminhtanmd@gmail.com>
2023-06-18 11:55:06 +0700

    Update Home.js
    Change IP

--- src/app/js/Home.js
+++ src/app/js/Home.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
 
   // ROS
   var ros = new ROSLIB.Ros({
-    url: 'ws://10.3.10.199:9090'
+    url: 'ws://192.168.1.201:9090'
   });
 
   ros.on('connection', function () {
